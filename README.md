c76fc3c7a4aeae7cb15f02659ed78dcb21b55fb0
Paulo Ricardo <dev.paulorc@gmail.com>
2024-03-17 22:21:13 -0300

    ✏️ CREATE

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Stopwatch
-Cronômetro com Classes em JavaScript.
+
+Cronômetro com JavaScript
+Este é um simples cronômetro criado com JavaScript, HTML e CSS. Ele permite que você inicie, pare e reinicie o cronômetro, exibindo o tempo decorrido em formato de horas, minutos e segundos.
+
+Arquivos
+
+index.html: Contém a estrutura HTML para a página do cronômetro.
+style.css: Arquivo CSS com estilos para o cronômetro e botões.
+StopWatch.js: Arquivo JavaScript com a classe StopWatch para controlar o cronômetro.
+
+
+Como usar
+
+Abra o arquivo index.html em um navegador da web.
+Clique no botão "Iniciar" para começar a contagem do tempo.
+Clique no botão "Parar" para pausar a contagem.
+Clique no botão "Reiniciar" para zerar o cronômetro.
+
+
+Personalização
+
+Você pode personalizar o estilo do cronômetro alterando as variáveis de cores e estilos no arquivo style.css. Além disso, é possível modificar o intervalo de atualização do cronômetro ajustando o valor de 1000 no método setInterval da classe StopWatch em StopWatch.js.
---
+++ Stopwatch.js
@@ -0,0 +1,70 @@
+class StopWatch {
+
+    #elapsedTimeInSeconds = 0;
+    #intervalId = null;
+
+    start(callback = () => {}) {
+        this.#intervalId = setInterval(() => {
+            this.#elapsedTimeInSeconds++;
+            callback();
+        } ,1000)
+
+    };
+    stop(callback = () => {}) {
+        clearInterval(this.#intervalId)
+        callback();
+    };
+    reset(callback = () => {}) {
+        this.#elapsedTimeInSeconds = 0;
+        callback();
+    };
+
+    get elapsedTime(){
+        return StopWatch.formatTime(this.#elapsedTimeInSeconds)
+    }
+
+    static formatTime(timeInSeconds) {
+        const hours = Math.floor(timeInSeconds / 3600);
+        const minutes = Math.floor((timeInSeconds % 3600) / 60);
+        const seconds = timeInSeconds - hours * 3600 - minutes * 60;
+
+        return `${StopWatch.zeroPadding(hours)}: ${StopWatch.zeroPadding(minutes)}: ${StopWatch.zeroPadding(seconds)}`
+    }
+    static zeroPadding(originalNumber, digits = 2){
+        let stringNumber = String(originalNumber);
+        const zeroesRequired = digits - stringNumber.length;
+        if (zeroesRequired <= 0) {
+            return stringNumber
+        }
+
+        for (let counter = 0; counter < zeroesRequired; counter++){
+            stringNumber = `0${stringNumber}`
+        }
+
+        return stringNumber;
+    }
+};
+
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+const resetBtn = document.getElementById('reset')
+const stopwatchDisplay = document.getElementById('stopwatch-display')
+
+function updateDisplay(){
+    stopwatchDisplay.innerText = sw1.elapsedTime
+}
+
+const sw1 = new StopWatch();
+
+
+startBtn.addEventListener('click', () => {
+    sw1.start(updateDisplay);
+})
+
+stopBtn.addEventListener('click', () => {
+    sw1.stop();
+})
+
+resetBtn.addEventListener('click', () => {
+    sw1.reset(updateDisplay);
+})
